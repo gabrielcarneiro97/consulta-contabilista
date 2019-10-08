30f56abd77d449aad0767ee29e5d6e653837f25a
Gabriel Carneiro <gabriel.carneiro97@live.com>
2019-10-08 09:33:35 -0300

    Terminei o código do puppeteer

--- puppeteer.js
+++ puppeteer.js
@@ -2,19 +2,18 @@
 
 const puppeteer = require('puppeteer-core');
 const chromium = require('chromium');
-// const isDev = require('electron-is-dev');
-
-const { loginUrl } = require('./urls.json');
-const { loginSelectors } = require('./selectors.json');
+const isDev = require('electron-is-dev');
 
 const {
-  credenciaisPj,
-  credenciaisPf,
-} = require('./private');
+  loginUrl,
+  paginaConsulta1,
+  paginaConsulta2,
+} = require('./urls.json');
+const { loginSelectors, consultaSelectors } = require('./selectors.json');
 
 async function setBrowser() {
-  // const chromiumPath = isDev ? chromium.path : chromium.path.replace('app.asar', 'app.asar.unpacked');
-  return puppeteer.launch({ headless: false, executablePath: chromium.path });
+  const chromiumPath = isDev ? chromium.path : chromium.path.replace('app.asar', 'app.asar.unpacked');
+  return puppeteer.launch({ headless: false, executablePath: chromiumPath });
 }
 
 async function setPage(browser) {
@@ -23,10 +22,7 @@ async function setPage(browser) {
   return page;
 }
 
-async function login(browser, creds) {
-
-  const page = await browser.newPage();
-
+async function login(page, creds) {
   await page.goto(loginUrl, { waitUntil: 'networkidle0' });
 
   await page.evaluate(({
@@ -39,7 +35,7 @@ async function login(browser, creds) {
     cpfInput,
     cnpjInput,
     senhaInput,
-    LoginBtn,
+    loginBtn,
   }) => {
     $(tipoSelect).val(tipo);
     $(tipoSelect).change();
@@ -54,18 +50,91 @@ async function login(browser, creds) {
       $(cnpjInput).val(cnpj);
       $(cnpjInput).change();
     }
-
-    $(LoginBtn).click();
-
+    $(loginBtn).click();
   }, creds, loginSelectors);
-
 }
 
-async function main() {
+async function extrairNumPaginas(page) {
+  if (!page.url().startsWith(paginaConsulta2)) {
+    throw new Error('Pagina incorreta!');
+  }
+
+  const num = await page.evaluate(({ proximoBtn }) => {
+    const string = document.querySelector(proximoBtn).previousSibling.nodeValue;
+
+    return parseInt(string.split('de')[1], 10);
+  }, consultaSelectors);
+
+  return num;
+}
+
+async function proximaPagina(page, ultima) {
+  if (!page.url().startsWith(paginaConsulta2)) {
+    throw new Error('Pagina incorreta!');
+  }
+
+  const paginaNum = await page.evaluate(({ proximoBtn, pgInput }, ultimaPg) => {
+    const input = document.querySelector(pgInput);
+    const atual = parseInt(input.value, 10);
+    if (atual === ultimaPg) return 0;
+    const next = atual + 1;
+    document.querySelector(proximoBtn).click();
+    return next;
+  }, consultaSelectors, ultima);
+
+  if (paginaNum !== 0) await page.waitForNavigation({ waitUntil: 'networkidle0' });
+  return paginaNum;
+}
+
+async function acessaConsulta(page) {
+  await page.waitForNavigation({ waitUntil: 'networkidle0' });
+  await page.goto(paginaConsulta1, { waitUntil: 'networkidle0' });
+
+  await page.evaluate(({ consultaLink }) => {
+    document.querySelector(consultaLink).click();
+  }, consultaSelectors);
+
+  await page.waitForNavigation({ waitUntil: 'networkidle0' });
+}
+
+async function executaConsulta(page) {
+  const ultimaPagina = await extrairNumPaginas(page);
+  let pageNum = 1;
+  const dataArr = [];
+  do {
+    const pageData = await page.evaluate(({ tabela }) => { // eslint-disable-line
+      const tbEl = document.getElementsByTagName(tabela)[3];
+      const data = [];
+      for (let i = 1; i < tbEl.rows.length - 1; i += 1) {
+        const cnpj = tbEl.rows[i].cells[2].innerText;
+        const ie = tbEl.rows[i].cells[1].innerText;
+        const nome = tbEl.rows[i].cells[3].innerText;
+        data.push({ cnpj, ie, nome });
+      }
+      return data;
+    }, consultaSelectors);
+    dataArr.push(pageData);
+    pageNum = await proximaPagina(page, ultimaPagina); // eslint-disable-line
+  } while (pageNum !== 0);
+
+  return dataArr.flat();
+}
+
+async function start(credenciais) {
   const browser = await setBrowser();
+  const page = setPage(browser);
 
-  await login(browser, credenciaisPj);
+  await login(page, credenciais);
 
+  await acessaConsulta(page);
+
+  const dados = await executaConsulta(page);
+
+  await browser.close();
+
+  return dados;
 }
 
-main();
+module.exports = {
+  start,
+};
