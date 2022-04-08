a6e64b28dd8124dd67f21997aa4220e73ef2fb5f
Rafael Castro <rafael.castro@syngenta.com>
2022-04-08 17:18:19 -0300

    Update token

--- google-api.js
+++ google-api.js
@@ -75,9 +75,9 @@ function authorize(callback) {
       return;
     }
     const credentials = JSON.parse(content);
-    const clientSecret = credentials.installed.client_secret;
-    const clientId = credentials.installed.client_id;
-    const redirectUrl = credentials.installed.redirect_uris[0];
+    const clientSecret = credentials.web.client_secret;
+    const clientId = credentials.web.client_id;
+    const redirectUrl = credentials.web.redirect_uris[0];
     const oauth2Client = new OAuth2Client(clientId, clientSecret, redirectUrl);
 
     // Check if we have previously stored a token.
